a839f9d2fe8037dad86a00a5073698e650ed07b5
hars-hj <harsh990921992@gmail.com>
2025-10-29 18:36:29 +0530

    Redirect root route to '/listing'

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.get("/", (req, res) => res.send("App started"));
+app.get("/", (req, res) => res.redirect("/listing"));
 
 app.use("/listing", listingsRoutes);
 app.use("/listing/:id/review", reviewsRoutes);
